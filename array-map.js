39390294e937238ab062bea8f47f49c8d253a27e
navyashree16 <navyashreeh162001>
2023-07-19 10:58:25 +0530

    this is map func

---
+++ array-map.js
@@ -0,0 +1,6 @@
+const fruits = ['apple', 'oranges', '  ', 'mango', '  ', 'lemon'];
+// for (let fruit of fruits) {
+//     console.log(fruit);
+
+    console.log(fruits.map(fruit => 'Fruit: ' + fruit));
+// }
